b198a19e583f96a4bf2d624270a2e2e0314e2958
Dustin De Luna <deluna.dustin5@gmail.com>
2026-04-10 16:27:24 -0500

    Add product images

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_10_194315) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_10_210720) do
   create_table "order_items", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.integer "order_id", null: false
@@ -32,6 +32,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_10_194315) do
     t.string "category"
     t.datetime "created_at", null: false
     t.text "description"
+    t.string "image"
     t.string "name"
     t.decimal "price"
     t.integer "stock"
