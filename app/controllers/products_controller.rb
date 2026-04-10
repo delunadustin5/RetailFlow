daac934fdf5a62e7a7904d6732155aeb5ea0973e
Dustin De Luna <deluna.dustin5@gmail.com>
2026-04-10 14:13:17 -0500

    Add quantity support to cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,19 +8,51 @@ class ProductsController < ApplicationController
   end
 
   def add_to_cart
-    session[:cart] ||= []
-    session[:cart] << params[:id]
+    normalize_cart_session
+
+    product_id = params[:id].to_s
+    session[:cart][product_id] ||= 0
+    session[:cart][product_id] += 1
+
     redirect_to cart_path
   end
 
   def cart
-    ids = session[:cart] || []
-    @cart_products = Product.find(ids)
+    normalize_cart_session
+
+    @cart_items = session[:cart].map do |product_id, quantity|
+      product = Product.find_by(id: product_id)
+      next unless product
+
+      {
+        product: product,
+        quantity: quantity.to_i
+      }
+    end.compact
   end
 
   def remove_from_cart
-    session[:cart] ||= []
-    session[:cart].delete_at(session[:cart].index(params[:id])) if session[:cart].include?(params[:id])
+    normalize_cart_session
+
+    product_id = params[:id].to_s
+    session[:cart].delete(product_id)
+
     redirect_to cart_path
+  end
+
+  private
+
+  def normalize_cart_session
+    if session[:cart].is_a?(Array)
+      new_cart = {}
+      session[:cart].each do |product_id|
+        id = product_id.to_s
+        new_cart[id] ||= 0
+        new_cart[id] += 1
+      end
+      session[:cart] = new_cart
+    else
+      session[:cart] ||= {}
+    end
   end
 end
