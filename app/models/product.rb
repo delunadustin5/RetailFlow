615c1f05e4a816d1f68c875996c75557dea208a0
Dustin De Luna <deluna.dustin5@gmail.com>
2026-04-22 21:58:04 -0500

    Add product validations and error handling

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,9 @@
 class Product < ApplicationRecord
   has_many :order_items, dependent: :destroy
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :stock, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
